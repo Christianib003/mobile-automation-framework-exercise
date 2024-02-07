e1d72d36c3d0e3702b0fe6db6ad258a52740e1be
Christian <christianib003@gmail.com>
2024-02-07 15:14:58 +0200

    create maven project

---
+++ mobile/src/main/java/de/frameworktsr/App.java
@@ -0,0 +1,13 @@
+package de.frameworktsr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
