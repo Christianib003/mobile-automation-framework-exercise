c2b24cb2cb78615d67391f3082b72071de2abc31
Christian <christianib003@gmail.com>
2024-02-09 11:19:56 +0200

    create PropertyManager class

---
+++ src/test/java/de/frameworktsr/utils/PropertyManager.java
@@ -0,0 +1,39 @@
+package de.frameworktsr.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyManager {
+    // Initialize a variable of type Properties holding properties from config.properties
+    private static Properties props = new Properties();
+
+    // Create a method to fetch the properties from the config.properties file
+    public Properties getProperties() throws IOException {
+        InputStream inputStream = null;
+        String propFileName = "config.properties";
+
+        if(props.isEmpty()) {
+            try {
+                // Fetch properties from config.properties and assign them to inputStream variable
+                inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+                // Assign the fetched properties to props variable
+                props.load(inputStream);
+            }
+
+            // Catch and print the origin of the error if fetching doesn't work
+            catch(IOException e) {
+                e.printStackTrace();
+                throw e;
+            }
+
+            finally {
+                // Close inputStream is it has been successfully retreived
+                if(inputStream != null) 
+                    inputStream.close();
+            }
+        }
+        return props;
+    }
+}
