d6fcca3694ccfadc0de8e10de8a77933d136db39
Christian <christianib003@gmail.com>
2024-02-12 12:27:28 +0200

    remove unnecessary imports

--- src/test/java/de/frameworktsr/pages/BasePage.java
+++ src/test/java/de/frameworktsr/pages/BasePage.java
@@ -2,10 +2,7 @@ package de.frameworktsr.pages;
 
 import java.time.Duration;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import de.frameworktsr.utils.DriverManager;
