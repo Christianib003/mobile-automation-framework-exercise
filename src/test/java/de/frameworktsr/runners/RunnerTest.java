a037fc1a28c7d79cd3d314b1056d85de1a0c3e35
Christian <christianib003@gmail.com>
2024-02-12 12:40:00 +0200

    create feature file for drag and drop

--- src/test/java/de/frameworktsr/runners/RunnerTest.java
+++ src/test/java/de/frameworktsr/runners/RunnerTest.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/de/frameworktsr/app/features", glue = "de.frameworktsr.stepdefinitions", plugin = {
+@CucumberOptions(features = "src/test/resources/de/frameworktsr/features", glue = "de.frameworktsr.stepdefinitions", plugin = {
         "pretty", "html:target/reports/html-report.html", "json:target/reports/json-report.html" })
 public class RunnerTest {
 
---
+++ src/test/resources/de/frameworktsr/features/DragAndDrop.feature
@@ -0,0 +1,9 @@
+Feature: Drag And Drop
+
+    The user should be able to drag and drop elements
+
+    Scenario: Drag and drop textless circle
+    When the user navigates to Views
+    And the user naviagates to Drag and Drop page
+    And the user drags and drops the textless circle
+    Then the message "Dropped!" should be displayed
