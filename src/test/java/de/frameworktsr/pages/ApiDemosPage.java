8e73f2e2973bba23bca6e7bcc8373011aa36bb86
Christian <christianib003@gmail.com>
2024-02-13 10:36:27 +0200

    create DragAndDropPage, its elements, and methods

--- src/test/java/de/frameworktsr/pages/ApiDemosPage.java
+++ src/test/java/de/frameworktsr/pages/ApiDemosPage.java
@@ -7,26 +7,17 @@ import io.appium.java_client.AppiumBy;
 public class ApiDemosPage extends BasePage {
     By viewsLinkBy = AppiumBy.accessibilityId("Views");
     By dragAndDropLinkBy = AppiumBy.accessibilityId("Drag and Drop");
-    By textlessCircleBy = AppiumBy.id("io.appium.android.apis:id/drag_dot_1");
-    By dragAndDropMessageContainerBy = AppiumBy.id("io.appium.android.apis:id/drag_result_text");
+    
 
     public void navigateToviews() {
         click(viewsLinkBy);
     }
 
-    public void navigateToDragAndDropPage() {
+    public DragAndDropPage navigateToDragAndDropPage() {
         click(dragAndDropLinkBy);
+
+        return new DragAndDropPage();
     }
 
-    public void dragAndDropTextLessCircle() {
-        dragAndDropUsingElementId(textlessCircleBy, 800, 1300);
-        // dragAndDropUsingCoordinates(271, 790, 1000, 2507);
-    }
-
-    public Boolean isDragAndDropSuccessful(String expectedMessage) {
-        String actualMessage = getTextAttribute(dragAndDropMessageContainerBy);
-        checkSimilarityOfText(expectedMessage, actualMessage);
-
-        return true;
-    }
+   
 }
--- src/test/java/de/frameworktsr/stepdefinitions/DragAndDrop.java
+++ src/test/java/de/frameworktsr/stepdefinitions/DragAndDrop.java
@@ -1,11 +1,13 @@
 package de.frameworktsr.stepdefinitions;
 
 import de.frameworktsr.pages.ApiDemosPage;
+import de.frameworktsr.pages.DragAndDropPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class DragAndDrop {
     ApiDemosPage apiDemosPage = new ApiDemosPage();
+    DragAndDropPage dragDropPage;
 
     @When("the user navigates to Views")
     public void userNavigatesToViews() {
@@ -14,16 +16,16 @@ public class DragAndDrop {
 
     @When("the user naviagates to Drag and Drop page")
     public void userNavigatesToDragAndDropPage() {
-        apiDemosPage.navigateToDragAndDropPage();
+        dragDropPage = apiDemosPage.navigateToDragAndDropPage();
     }
 
     @When("the user drags and drops the textless circle")
     public void userDragsAndDropTextlessCircle() {
-        apiDemosPage.dragAndDropTextLessCircle();
+        dragDropPage.dragAndDropTextLessCircle();
     }
 
     @Then("the message {string} should be displayed")
     public void theMessageShouldBeDisplayed(String string) {
-        apiDemosPage.isDragAndDropSuccessful(string);
+        dragDropPage.isDragAndDropSuccessful(string);
     }
 }
