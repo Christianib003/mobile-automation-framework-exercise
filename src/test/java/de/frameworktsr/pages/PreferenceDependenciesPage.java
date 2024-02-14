946803db1401d94288657e79543a21b457cde0ef
Christian <christianib003@gmail.com>
2024-02-14 13:03:38 +0200

    add all stepdefinitions for adding new wifi settings

--- src/test/java/de/frameworktsr/pages/PreferenceDependenciesPage.java
+++ src/test/java/de/frameworktsr/pages/PreferenceDependenciesPage.java
@@ -1,5 +1,32 @@
 package de.frameworktsr.pages;
 
-public class PreferenceDependenciesPage {
-    
+import org.openqa.selenium.By;
+
+import io.appium.java_client.AppiumBy;
+
+public class PreferenceDependenciesPage extends BasePage {
+    By activateWifiSettingsCheckboxBy = AppiumBy.id("android:id/checkbox");
+    By wifiSettingsLinkBy = AppiumBy.xpath("//android.widget.TextView[@text='WiFi settings']");
+    By wifiSettingInputAreaBy = AppiumBy.id("android:id/edit");
+    By okBtnBy = AppiumBy.id("android:id/button1");
+
+    public void activateWifiSettings() {
+        click(activateWifiSettingsCheckboxBy);
+    }
+
+    public void openWifiSettingsPopUp() {
+        click(wifiSettingsLinkBy);
+    }
+
+    public void addNewWifiSetting() {
+        addTextToField(wifiSettingInputAreaBy, "RK");
+        click(okBtnBy);
+    }
+
+    public Boolean isWifiSettingSaved(String expectedWifiSetting) {
+        String currentWifiSetting = getAttributeValue(wifiSettingInputAreaBy, "text");
+        checkSimilarityOfText(currentWifiSetting, expectedWifiSetting);
+
+        return true;
+    }
 }
