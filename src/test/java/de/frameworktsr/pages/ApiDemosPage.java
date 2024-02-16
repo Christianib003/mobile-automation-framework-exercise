8c601ab5ac402d1ec1eb9b977e4235368546b7ac
Christian <christianib003@gmail.com>
2024-02-16 12:55:16 +0200

    remove unnecessary imports

--- src/test/java/de/frameworktsr/pages/ApiDemosPage.java
+++ src/test/java/de/frameworktsr/pages/ApiDemosPage.java
@@ -1,8 +1,6 @@
 package de.frameworktsr.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.AppiumBy;
 
