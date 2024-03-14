9979a30e6b1d49aec04007057d5dd1f3349743e0
Christian <christianib003@gmail.com>
2024-03-14 09:39:56 +0200

    define scenario steps for dropdown feature

---
+++ src/test/java/de/frameworktsr/stepdefinitions/Dropdown.java
@@ -0,0 +1,23 @@
+package de.frameworktsr.stepdefinitions;
+
+import io.cucumber.java.en.When;
+
+public class Dropdown {
+    @When("the user navigates to animation")
+    public void the_user_navigates_to_animation() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user navigates to Push")
+    public void the_user_navigates_to_push() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user selects their preferred {string}")
+    public void the_user_selects_their_preferred(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/de/frameworktsr/features/Dropdown.feature
@@ -0,0 +1,13 @@
+Feature: Dropdown
+
+    As a user
+    I should be able to select from the Dropdown
+    so that I can see the animation I want
+
+    @current
+    Scenario: Select animation
+        When the user navigates to Views
+        And the user navigates to animation
+        And the user navigates to Push
+        And the user selects their preferred "<animation_type>"
+        Then the "<animation_type>" should be selected
